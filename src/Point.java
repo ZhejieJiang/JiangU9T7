9b4a8c72f55bd3b09a15f9b439ef645e85350dea
Zhejie Jiang <zhejiej@nycstudents.net>
2023-03-31 09:31:14 -0400

    working

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Point {
+public class Point
+{
+    private int x;
+    private int y;
+    private String label;
+
+    public Point(String label, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
