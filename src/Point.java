4dd3d5a7d6b881e6e62da2573b30a7a22c744d3d
Zhejie Jiang <zhejiej@nycstudents.net>
2023-04-03 08:38:41 -0400

    finish

--- src/Point.java
+++ src/Point.java
@@ -1,16 +1,24 @@
-public class Point
-{
+public class Point {
     private int x;
     private int y;
     private String label;
-
     public Point(String label, int x, int y) {
+        this.label = label;
         this.x = x;
         this.y = y;
-        this.label = label;
+    }
+    @Override
+    public String toString()
+    {
+        return "Point " + label + " is at x = " + x + " y = "+ y;
     }
 
-    public String toString() {
-        return "(" + x + ", " + y + ")";
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Point)) {
+            return false;
+        }
+    Point otherPoint = (Point) obj;
+    return (this.x == otherPoint.x) && (this.y == otherPoint.y);
     }
 }
